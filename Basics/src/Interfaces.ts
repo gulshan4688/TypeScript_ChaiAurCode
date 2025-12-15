b1b029a10e42d41d9d7a8e45fe1840789d0e9e12
GulshanKumarM <gulshan.kumar@mantratec.com>
2025-12-15 16:18:04 +0530

    interface and objects

--- Basics/src/Interfaces.ts
+++ Basics/src/Interfaces.ts
@@ -1,9 +1,3 @@
-type ChaiOrder = {
-    type:string;
-    sugar: number;
-    strong: boolean;
-};
-
 // this is a way normal way to pass parameters but what if they are repeating them selves
 function makeChai(order:{type:string; sugar: number; strong: boolean}){
     console.log(order);
@@ -12,3 +6,114 @@ function makeChai(order:{type:string; sugar: number; strong: boolean}){
 function serveChai(order:{type: string; sugar:number; strong: boolean}){
     console.log(order);
 }
+
+type ChaiOrder = {
+    type:string;
+    sugar: number;
+    strong: boolean;
+};
+
+// but let us create this and we can use this ChaiOrder type function like an order parameter 
+
+function makeChai2(order : ChaiOrder){
+    console.log(order);
+}
+function serveChai2(order:ChaiOrder){
+    console.log(order);
+}
+
+
+type teaRecipe = {
+    water:number;
+    milk:number;
+}
+
+class MasalaChai implements teaRecipe{
+    water = 100;
+    milk = 50;
+}
+
+type cupSize = "small" | "large" ;
+
+// A class can only implement an object type or intersection of object types with statically known members.ts(2422)
+// class Chai implements cupSize{
+
+// }
+
+// eg how 
+interface  ChaiOrder2 {
+    type:string;
+    sugar: number;
+    strong: boolean;
+};
+
+// now we have this variable like cupSize  "type cupSize = "small" | "large" ;", if we want to make this as Interface than 
+
+interface cupSize2{
+    size: "small" | "medium" | "large" ;
+}
+
+// so to declare any interface we need to declare it like an object we cant just write interface anywhere
+
+//  time to implement intereface in a class 
+
+class Chai2 implements cupSize2 {
+    size: "small"| "medium" | "large" = "large";
+}
+
+type Response = { ok:true; } | { ok:false; }
+
+// class myRes implements Response {
+//     ok : boolean = true;
+// }
+
+// class gives error while implementing as type , classes only needs Interfaces 
+
+// union of types
+type teaType = "masala" | "ginger" | "lemon";
+
+function orderChai3(t : teaType){
+    console.log(t);
+}
+
+type baseChai = {
+    teaLeaves : number
+} 
+
+type extra = {
+    masala : number
+}
+
+type masalaChai = baseChai & extra;
+
+const cup : masalaChai = {
+    teaLeaves : 2,
+    masala : 1
+}
+
+// optoinal values 
+type User = {
+    username : string ;
+    bio? : String 
+}
+const u1 : User ={
+    username : "mani"
+}
+const u2 : User = {
+    username : "mani2",
+    bio: "hnji kaise hai app log "
+}
+
+// its not mandatory to put both values , its our choice
+
+type config = {
+    readonly appName : string
+    version : number
+}
+
+const cfg : config = {
+    appName : 'massterji',
+    version : 1
+}
+
+// cfg.appName = "yoyo"  // its readonly 
---
+++ Basics/src/objects.ts
@@ -0,0 +1,121 @@
+// const chai = {
+//     name: "masala Chai",
+//     price: 20,
+//     isHot: true
+// }
+
+// // declaring object types 
+
+// let tea: {
+//     name: string,
+//     price: number,
+//     isHot: boolean
+// }
+// tea = {
+//     name: "ginger tea",
+//     price: 25,
+//     isHot: true
+// }
+
+// // alias objects
+
+// type Tea = {
+//     name: string,
+//     price: number,
+//     ingredients: string[]
+// }
+
+// const adrakChai: Tea = {
+//     name: "adrak Chai",
+//     price: 23,
+//     ingredients: ["gingder", "chaiPatti"]
+// }
+
+// type cup = {
+//     size: string
+// }
+
+// let smallCup: cup = {
+//     size: "200ml"
+// }
+// let largeCup = {
+//     size: "300ml",
+//     isBig: true
+// }
+// smallCup = largeCup;
+
+// type brew = { brewTime: number }
+// const coffee = { brewTime: 8, beans: "arabica" }
+// const chaiBrew: brew = coffee;
+
+
+// type user = {
+//     username1: string;
+//     password1: string;
+// }
+
+// const u3: user = {
+//     username1: "mani",
+//     password1: "123"
+// }
+
+// type Item = { name1: string, quantity: number }
+// type Address = { street: string, pin: number }
+
+// type Order = {
+//     id: string;
+//     items: Item[];   // here items has Item[] type means in here we can put many elements inside this array which contains name1 and quantity 
+//     address: Address;
+// }
+
+type Chai = {
+    name: string;
+    price: number;
+    isHot: boolean;
+}
+
+const updatedChai = (updates: Partial<Chai>) => {
+    console.log("updating chai with ", updates);
+}
+
+updatedChai({price:25});
+updatedChai({isHot:false});
+
+type ChaiOrder ={
+    name?: string;
+    quantity?:number;
+}
+const placeOrder = (order : Required<ChaiOrder>) =>{
+    console.log(order);
+}
+
+placeOrder({name:"mani",quantity:23});
+
+type Chai1 ={
+    name: string;
+    price: number;
+    isHot:boolean;
+    ingredients: string[]
+}
+
+type basicChaiInfo = Pick < Chai1,"name" | "price" > ;  // only pick these properties from type while making the object 
+
+const ChaiInfo:basicChaiInfo ={
+    name: "lemon Tea",
+    price : 30
+}
+
+type ChaiNew ={
+    name: string;
+    price: number;
+    isHot:boolean;
+    secretIngredient: string;
+}
+
+type PublicChai = Omit<ChaiNew , "secretIngredient" > ; // means while making an object of type PublicChai we dont have to use this "sercretIngredients"
+
+const BestChai :PublicChai ={
+    name: "lemon Tea",
+    price : 30,
+    isHot:true    
+}  // above as u can see BestChai is of type PublicChai in which we can omit one variable 
